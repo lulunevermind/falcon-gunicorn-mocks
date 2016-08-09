befbee385f105f9f32a86a017830a65dc1b57d5b
sergey.kudasov <sergey.kudasov@rtlabs.ru>
2016-08-09 14:24:26 +0300

    headers

--- default_handler.py
+++ default_handler.py
@@ -12,11 +12,16 @@ class DefaultHandler(object):
         logging.info('Route triggered -->> %s' % req.path)
         for expectations in self.mappings:
             for expectation, expressions in expectations.items():
+                #from here we parsing expectation line like {"contains": ["<dob:usrF>simple</dob:usrF>"],
+                #  "request": "mvd_simple.req", "response": "mvd_simple.resp",
+                #"response-header": {"Content-Type": "text/xml; charset=utf-8", "Accept": "ALL"}}
+                response_headers = expectations.get("response-header")
                 if expectation == 'contains':
                     for exp in expressions:
                         if body.__contains__(exp.encode('utf-8')):
                             response = expectations['response']
                             resp.body = response
                             logging.info('Match for %s succeded!' % exp.encode('utf-8'))
-                            logging.info('Response:\n%s' % response)
-                            resp.append_header("Content-Type", "text/xml; charset=utf-8")
+                            logging.info('Setting headers: %s' % response_headers)
+                            for k, v in response_headers.items():
+                                resp.append_header(k, v)
